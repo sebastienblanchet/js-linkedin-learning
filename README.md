c9e7346fb344e2e60e2f5175750e37f56fdc69ae
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-30 15:01:02 -0400

    Chapter 8: typing speed ex

---
+++ README.md
@@ -0,0 +1,18 @@
+# JavaScript Linkedin Learning
+
+### Sebastien Blanchet
+
+Tracking developments of previously complete JavaScript course.
+
+## Tags
+
+<!-- * [v12.0](https://github.com/sebastienblanchet/js-linkedin-learning/releases/tag/v12.0): Chapter 12: Bonus
+* [v11.0](https://github.com/sebastienblanchet/js-linkedin-learning/releases/tag/v11.0): Chapter 11: QA
+* [v10.0](https://github.com/sebastienblanchet/js-linkedin-learning/releases/tag/v10.0): Chapter 10: Automated image markup
+* [v9.0](https://github.com/sebastienblanchet/js-linkedin-learning/releases/tag/v9.0): Chapter 9: Loops -->
+* [v8.0](https://github.com/sebastienblanchet/js-linkedin-learning/releases/tag/v8.0): Chapter 8: Typing speed example
+* [v7.0](https://github.com/sebastienblanchet/js-linkedin-learning/releases/tag/v7.0): Chapter 7: DOM events
+* [v6.0](https://github.com/sebastienblanchet/js-linkedin-learning/releases/tag/v6.0): Chapter 6: analog clock example
+* [v5.0](https://github.com/sebastienblanchet/js-linkedin-learning/releases/tag/v5.0): Chapter 5: Basic js DOM intro
+* [v4.0](https://github.com/sebastienblanchet/js-linkedin-learning/releases/tag/v4.0): Chapter 4: Functions
+* [v3.0](https://github.com/sebastienblanchet/js-linkedin-learning/releases/tag/v3.0): Chapter 3: Data
--- src/script.js
+++ src/script.js
@@ -1,28 +1,73 @@
-const HOURHAND = document.querySelector("#hour");
-const MINUTEHAND = document.querySelector("#minute");
-const SECONDHAND = document.querySelector("#second");
+const testWrapper = document.querySelector(".test-wrapper");
+const testArea = document.querySelector("#test-area");
+const originText = document.querySelector("#origin-text p").innerHTML;
+const resetButton = document.querySelector("#reset");
+const theTimer = document.querySelector(".timer");
 
-var date = new Date();
-console.log(date);
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+var timer = [0, 0, 0, 0];
+var interval;
+var timerRunning = false;
 
-let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
-let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
-let secPosition = sec * 360 / 60;
+// Add leading zero to numbers 9 or below (purely for aesthetics):
+function leadingZero(time) {
+  if (time <= 9) {
+    time = "0" + time;
+  }
+  return time;
+}
 
-function runTheClock() {
+// Run a standard minute/second/hundredths timer:
+function runTimer() {
+  let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+  theTimer.innerHTML = currentTime;
+  timer[3]++;
 
-  hrPosition = hrPosition + (3 / 360);
-  minPosition = minPosition + (6 / 60);
-  secPosition = secPosition + 6;
+  timer[0] = Math.floor((timer[3] / 100) / 60);
+  timer[1] = Math.floor((timer[3] / 100) - (timer[0] * 60));
+  timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+}
 
-  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+// Match the text entered with the provided text on the page:
+function spellCheck() {
+  let textEntered = testArea.value;
+  let originTextMatch = originText.substring(0, textEntered.length);
+
+  if (textEntered == originText) {
+    clearInterval(interval);
+    testWrapper.style.borderColor = "#429890";
+  } else {
+    if (textEntered == originTextMatch) {
+      testWrapper.style.borderColor = "#65CCf3";
+    } else {
+      testWrapper.style.borderColor = "#E95D0F";
+    }
+  }
 
 }
 
-var interval = setInterval(runTheClock, 1000);
+// Start the timer:
+function start() {
+  let textEnterdLength = testArea.value.length;
+  if (textEnterdLength === 0 && !timerRunning) {
+    timerRunning = true;
+    interval = setInterval(runTimer, 10);
+  }
+  console.log(textEnterdLength);
+}
+
+// Reset everything:
+function reset() {
+  clearInterval(interval);
+  interval = null;
+  timer = [0, 0, 0, 0];
+  timerRunning = false;
+
+  testArea.value = "";
+  theTimer.innerHTML = "00:00:00";
+  testWrapper.style.borderColor = "grey";
+}
+
+// Event listeners for keyboard input and the reset
+testArea.addEventListener("keypress", start, false);
+testArea.addEventListener("keyup", spellCheck, false);
+resetButton.addEventListener("click", reset, false);
